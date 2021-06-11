428e8e1374074351d2532d3fa5db83c18e658a8e
pragadaarun <pmaniarunkumar@gmail.com>
2021-06-11 22:35:42 +0530

    Added : Profile Fragment Layout

---
+++ app/src/main/java/com/bridgeLabz/bookstore/UI/Fragments/ProfileFragment.java
@@ -0,0 +1,25 @@
+package com.bridgeLabz.bookstore.UI.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.bridgeLabz.bookstore.R;
+
+public class ProfileFragment extends Fragment {
+
+    private TextView profileNameTextView, emailTextView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+
+    }
+}
